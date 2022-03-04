de88dade29beae62ca8d4528ec3760ac386c2216
Octane0411 <wdznb1@gmail.com>
2022-03-04 11:12:55 +0800

    引入bigcache

--- server/router/api/v1/auth.go
+++ server/router/api/v1/auth.go
@@ -1,6 +1,8 @@
 package v1
 
 import (
+	"g-server/common/errcode"
+	"g-server/common/response"
 	"g-server/utils/email"
 	"github.com/gin-gonic/gin"
 )
@@ -13,7 +15,8 @@ func Register(c *gin.Context) {
 	//captchaStr := captcha.GetCaptcha()
 	emailAddr := c.Query("email")
 	if ok := email.ValidEmail(emailAddr); !ok {
-
+		r := response.NewResponse(c)
+		r.ToErrorResponse(errcode.InvalidParams)
 	}
 }
 
